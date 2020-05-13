65aeb625e0ca7cbbf13ed71be7b0b7a6b10c32dc
yu.zhang <yu.zhang@agrant.cn>
2020-05-13 19:05:21 +0800

    规范下返回的实体类

--- src/main/java/com/cribbee/gofree/util/ResultMsg.java
+++ src/main/java/com/cribbee/gofree/util/ResultMsg.java
@@ -1,5 +1,7 @@
 package com.cribbee.gofree.util;
 
+import lombok.Data;
+
 /**
  * ClassName: ResultMsg
  * Description: TODO
@@ -7,10 +9,16 @@ package com.cribbee.gofree.util;
  * Date: 2020/2/19、3:16 PM
  * Version: 1.0
  **/
+
+@Data
 public class ResultMsg {
     private int code;
     private String msg;
     private Object data;
+
+    public ResultMsg(){
+
+    }
 
     public ResultMsg(int Code, String Msg, Object Data)
     {
@@ -18,22 +26,5 @@ public class ResultMsg {
         this.msg = Msg;
         this.data = Data;
     }
-    public int getErrcode() {
-        return code;
-    }
-    public void setErrcode(int code) {
-        this.code = code;
-    }
-    public String getErrmsg() {
-        return msg;
-    }
-    public void setErrmsg(String msg) {
-        this.msg = msg;
-    }
-    public Object getP2pdata() {
-        return data;
-    }
-    public void setP2pdata(Object data) {
-        this.data = data;
-    }
+
 }
