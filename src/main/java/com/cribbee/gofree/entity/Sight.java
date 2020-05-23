fa051ae2f301292d13d744e0450d04c97c2f0b4e
cribbee <cribbee66@gmail.com>
2020-05-24 01:40:38 +0800

    create table of sight

--- src/main/java/com/cribbee/gofree/entity/Sight.java
+++ src/main/java/com/cribbee/gofree/entity/Sight.java
@@ -21,20 +21,50 @@ public class Sight {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    //所属区
-    private Integer region_id;
+    private String sight_name;
 
+    //所属城市
+    private Integer city_id;
 
-    private String name;
+    //综合打分
+    private Double score;
+
+    //综合打分排名
+    private Integer rank;
+
+    //评论数量
+    private Integer cmt_num;
 
     //标签
     private String tag;
 
+    //价格
+    private Double price;
+
 
     private String addr;
 
-    //价格
-    private Double price;
+    //路径
+    private String url;
+
+    //主图
+    private String pic1;
+
+    //副图
+    private String pic2;
+
+    //景点信息
+    private String info;
+
+    //开发时间
+    private String open_time;
+
+    //景点描述
+    private String sight_desc;
+
+    //小tips
+    private String tips;
+
 
     //经度
     private Double lng;
@@ -42,38 +72,12 @@ public class Sight {
     //维度
     private Double lat;
 
-    //评论数量
-    private Integer cmt_num;
 
-    //景点描述
-    private String sight_desc;
-
-    //综合打分
-    private Double score;
-
-
-    private String tel;
-
-    //耗时
-    private String time_cost;
-
-    //交通方式
-    private String trans;
-
-    //开放时间
-    private String open_time;
-
-    //景点图片
-    private String main_image;
-
-    //路径
-    private String url;
-
-    //所属景点id
-    private Integer parent_id;
-
-
-    private String child_id;
+//    //所属景点id
+//    private Integer parent_id;
+//
+//
+//    private String child_id;
 
 
     public Sight() {
@@ -83,24 +87,23 @@ public class Sight {
     public String toString() {
         return "Sight{" +
                 "id=" + id +
-                ", region_id=" + region_id +
-                ", name='" + name + '\'' +
+                ", sight_name='" + sight_name + '\'' +
+                ", city_id=" + city_id +
+                ", score=" + score +
+                ", rank=" + rank +
+                ", cmt_num=" + cmt_num +
                 ", tag='" + tag + '\'' +
-                ", addr='" + addr + '\'' +
                 ", price=" + price +
+                ", addr='" + addr + '\'' +
+                ", url='" + url + '\'' +
+                ", pic1='" + pic1 + '\'' +
+                ", pic2='" + pic2 + '\'' +
+                ", info='" + info + '\'' +
+                ", open_time='" + open_time + '\'' +
+                ", sight_desc='" + sight_desc + '\'' +
+                ", tips='" + tips + '\'' +
                 ", lng=" + lng +
                 ", lat=" + lat +
-                ", cmt_num=" + cmt_num +
-                ", sight_desc='" + sight_desc + '\'' +
-                ", score=" + score +
-                ", tel='" + tel + '\'' +
-                ", time_cost='" + time_cost + '\'' +
-                ", trans='" + trans + '\'' +
-                ", open_time='" + open_time + '\'' +
-                ", main_image='" + main_image + '\'' +
-                ", url='" + url + '\'' +
-                ", parent_id=" + parent_id +
-                ", child_id='" + child_id + '\'' +
                 '}';
     }
 
@@ -112,20 +115,44 @@ public class Sight {
         this.id = id;
     }
 
-    public Integer getRegion_id() {
-        return region_id;
+    public String getSight_name() {
+        return sight_name;
     }
 
-    public void setRegion_id(Integer region_id) {
-        this.region_id = region_id;
+    public void setSight_name(String sight_name) {
+        this.sight_name = sight_name;
     }
 
-    public String getName() {
-        return name;
+    public Integer getCity_id() {
+        return city_id;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setCity_id(Integer city_id) {
+        this.city_id = city_id;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
+    public Integer getRank() {
+        return rank;
+    }
+
+    public void setRank(Integer rank) {
+        this.rank = rank;
+    }
+
+    public Integer getCmt_num() {
+        return cmt_num;
+    }
+
+    public void setCmt_num(Integer cmt_num) {
+        this.cmt_num = cmt_num;
     }
 
     public String getTag() {
@@ -136,6 +163,14 @@ public class Sight {
         this.tag = tag;
     }
 
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
     public String getAddr() {
         return addr;
     }
@@ -144,12 +179,60 @@ public class Sight {
         this.addr = addr;
     }
 
-    public Double getPrice() {
-        return price;
+    public String getUrl() {
+        return url;
     }
 
-    public void setPrice(Double price) {
-        this.price = price;
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getPic1() {
+        return pic1;
+    }
+
+    public void setPic1(String pic1) {
+        this.pic1 = pic1;
+    }
+
+    public String getPic2() {
+        return pic2;
+    }
+
+    public void setPic2(String pic2) {
+        this.pic2 = pic2;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public String getOpen_time() {
+        return open_time;
+    }
+
+    public void setOpen_time(String open_time) {
+        this.open_time = open_time;
+    }
+
+    public String getSight_desc() {
+        return sight_desc;
+    }
+
+    public void setSight_desc(String sight_desc) {
+        this.sight_desc = sight_desc;
+    }
+
+    public String getTips() {
+        return tips;
+    }
+
+    public void setTips(String tips) {
+        this.tips = tips;
     }
 
     public Double getLng() {
@@ -166,93 +249,5 @@ public class Sight {
 
     public void setLat(Double lat) {
         this.lat = lat;
-    }
-
-    public Integer getCmt_num() {
-        return cmt_num;
-    }
-
-    public void setCmt_num(Integer cmt_num) {
-        this.cmt_num = cmt_num;
-    }
-
-    public String getSight_desc() {
-        return sight_desc;
-    }
-
-    public void setSight_desc(String sight_desc) {
-        this.sight_desc = sight_desc;
-    }
-
-    public Double getScore() {
-        return score;
-    }
-
-    public void setScore(Double score) {
-        this.score = score;
-    }
-
-    public String getTel() {
-        return tel;
-    }
-
-    public void setTel(String tel) {
-        this.tel = tel;
-    }
-
-    public String getTime_cost() {
-        return time_cost;
-    }
-
-    public void setTime_cost(String time_cost) {
-        this.time_cost = time_cost;
-    }
-
-    public String getTrans() {
-        return trans;
-    }
-
-    public void setTrans(String trans) {
-        this.trans = trans;
-    }
-
-    public String getOpen_time() {
-        return open_time;
-    }
-
-    public void setOpen_time(String open_time) {
-        this.open_time = open_time;
-    }
-
-    public String getMain_image() {
-        return main_image;
-    }
-
-    public void setMain_image(String main_image) {
-        this.main_image = main_image;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public Integer getParent_id() {
-        return parent_id;
-    }
-
-    public void setParent_id(Integer parent_id) {
-        this.parent_id = parent_id;
-    }
-
-    public String getChild_id() {
-        return child_id;
-    }
-
-    public void setChild_id(String child_id) {
-        this.child_id = child_id;
     }
 }
